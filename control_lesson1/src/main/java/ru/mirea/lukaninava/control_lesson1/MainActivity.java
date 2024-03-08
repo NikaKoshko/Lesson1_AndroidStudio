32cac4eb6cda4447fc1503bd7c23c96114e94bb1
Veronica <nikakoshko@gmail.com>
2024-03-09 01:28:33 +0300

    Lesson1

--- control_lesson1/src/main/java/ru/mirea/lukaninava/control_lesson1/MainActivity.java
+++ control_lesson1/src/main/java/ru/mirea/lukaninava/control_lesson1/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_second);
 
         TextView tv = findViewById(R.id.textViewStudent);
         TextView myTextView = (TextView) findViewById(R.id.textViewStudent);
